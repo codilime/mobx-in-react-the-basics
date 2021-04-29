f4f7cd379281857c6da706a11e905fdfd94636ca
Piotr Gejgalis <piotr@gejgalis.com>
2021-04-29 14:40:14 +0200

    Removed code which should not be in demo

--- src/demo04/Demo04-console.ts
+++ src/demo04/Demo04-console.ts
@@ -1,18 +1,14 @@
-import { autorun, observable, runInAction } from "mobx";
+import { autorun, observable } from "mobx";
 
 const friends = observable([
   { fullName: "Ross Geller" },
   { fullName: "Rachel Green" },
-  { fullName: "Joey Trribbiani" },
+  { fullName: "Joey Tribbiani" },
 ]);
 
 autorun(() => {
   console.log("Friends from autorun():");
   console.log(friends.map((f) => f.fullName).join(", \n"));
-});
-
-runInAction(() => {
-  friends[0].fullName = "Chandler Bing";
 });
 
 window.friends = friends;
